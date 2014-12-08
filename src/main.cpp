ce9b532b8e5935fb184aa4c90daca7c6246a9e0d
William Kerrigan <Deadleg@gmail.com>
2014-12-08 15:33:44 +1300

    Added logMessageHandler to write to log file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDateTime>
+#include <QTextStream>
+#include <QFile>
+#include <QtDebug>
 
+void logMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QString message;
+    QString currentDT = QDateTime::currentDateTime().toString("yyyy/mm/dd hh:mm:ss");
+    switch (type) {
+    case QtDebugMsg:
+        message = QString("%1 - DEBUG - %2\n").arg(currentDT, msg);
+        break;
+    case QtWarningMsg:
+        message = QString("%1 - WARNING - %2\n").arg(currentDT, msg);
+        break;
+    case QtCriticalMsg:
+        message = QString("%1 - CRITICAL - %2\n").arg(currentDT, msg);
+        break;
+    case QtFatalMsg:
+        message = QString("%1 - FATAL - %2\n").arg(currentDT, msg);
+    }
+    QFile logFile("QuickFolderCloner.log");
+    logFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream stream(&logFile);
+    stream << message;
+}
 
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(logMessageHandler);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
